ba922b0ad30e35790efc6c1276151e6ade7214b9
IbragimAbduragimov <ibragimabduragimov666@gmail.com>
2026-01-01 19:04:20 +0300

    fixed

--- drink.js
+++ drink.js
@@ -1,3 +1,4 @@
+//создать абстрактный класс drink
 class Drink {
   #temperature
   constructor(name, size, price, temperature) {
@@ -26,6 +27,8 @@ class Drink {
     console.log(`напиток готов, можете забирать!`)
   }
 }
+
+//создать от них наследников
 class Limonad extends Drink {
   constructor(name, size, price, temperature, carbonation, color) {
     super(name, size, price, temperature)
@@ -62,25 +65,9 @@ class Soda extends Drink {
   }
 }
 
-
+//создать переменный на основе ранних наследников
 const limonad = new Limonad('холодный лимонад', '400', '2', '0', 'среднегазированный', 'зеленый')
 const coffee = new Coffee('экспрессо', '200', '1.5', '0', 'средняя обжарка', 'крепкий')
 const tea = new Tea('чай с лимоном', '350', '1', '0', 'сладкий', 'некрепкий')
 const soda = new Soda('газированный напиток', '450', '0.5', '0', 'сильногазированный', 'клубничный')
 
-
-class Cafe {
-  constructor(name, location) {
-    this.name = name;
-    this.location = location
-  }
-  getInfo() {
-    console.log(`${this.name} - это кафе, которая основана магнатом Владиславом Бабенко
-     и находиться это кафе по адресу ${this.location}.`);
-  }
-  orderDrink(drink) {
-    drink.giveDrink()
-    console.log(`в ${this.name} сделали заказ на ${this.drink.name}`)
-  }
-}
-const CafeVladislava = new Cafe('Кафе у моря', 'Дагестан, город Буйнакск...')
